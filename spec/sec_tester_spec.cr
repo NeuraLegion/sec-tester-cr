c53645483f2e37d9be9b02b1ae7a1e96b473b33a
Bar Hofesh <bar.hofesh@gmail.com>
2021-07-22 19:51:26 +0300

    add XSS test

--- spec/sec_tester_spec.cr
+++ spec/sec_tester_spec.cr
@@ -3,7 +3,7 @@ require "http"
 require "uri"
 
 describe SecTester::Test do
-  it "starts a new scan" do
+  it "starts a new scan for OSI" do
     server = HTTP::Server.new do |context|
       command = URI.decode_www_form(context.request.query_params["command"]?.to_s)
 
@@ -25,6 +25,33 @@ describe SecTester::Test do
 
     tester = SecTester::Test.new
     tester.run_check(scan_name: "UnitTestingScan", test_name: "osi", target_url: "http://#{addr}/?command=time")
+  ensure
+    server.try &.close
+    tester.try &.cleanup
+  end
+
+  it "starts a new scan for XSS" do
+    server = HTTP::Server.new do |context|
+      name = URI.decode_www_form(context.request.query_params["name"]?.to_s)
+
+      context.response.content_type = "text/html"
+      context.response << <<-EOF
+        <html>
+          <body>
+            <h1>Hello, world!</h1>
+            <p>#{name}</p>
+          </body>
+        </html>
+        EOF
+    end
+
+    addr = server.bind_unused_port
+    spawn do
+      server.listen
+    end
+
+    tester = SecTester::Test.new
+    tester.run_check(scan_name: "UnitTestingScan", test_name: "xss", target_url: "http://#{addr}/?name=jhon")
   ensure
     server.try &.close
     tester.try &.cleanup
