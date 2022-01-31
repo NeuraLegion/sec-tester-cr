3db847293d0bd9540af22c6a74fe953d61985050
Bar Hofesh <bar.hofesh@gmail.com>
2022-01-31 21:34:31 +0200

    reduced CI

--- spec/sec_tester_spec.cr
+++ spec/sec_tester_spec.cr
@@ -3,39 +3,6 @@ require "http"
 require "uri"
 
 describe SecTester::Test do
-  it "starts a new scan for OSI" do
-    server = HTTP::Server.new do |context|
-      command = URI.decode_www_form(context.request.query_params["command"]?.to_s)
-
-      context.response.content_type = "text/html"
-      context.response << <<-EOF
-        <html>
-          <body>
-            <h1>Hello, world!</h1>
-            <p>#{`#{command}`}</p>
-          </body>
-        </html>
-        EOF
-    end
-
-    addr = server.bind_unused_port
-    spawn do
-      server.listen
-    end
-
-    tester = SecTester::Test.new
-    expect_raises(SecTester::IssueFound) do
-      tester.run_check(
-        scan_name: "UnitTestingScan - OSI",
-        test_name: "osi",
-        target: SecTester::Target.new("http://#{addr}/?command=time")
-      )
-    end
-  ensure
-    server.try &.close
-    tester.try &.cleanup
-  end
-
   it "starts a new scan for XSS" do
     server = HTTP::Server.new do |context|
       name = URI.decode_www_form(context.request.query_params["name"]?.to_s)
